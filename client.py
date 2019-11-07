4c1fca1b471d09ea9412e0fe01d36312137a6297
Yarosvet <yarosvetk@yandex.ru>
2019-11-08 01:31:57 +0300

    Осталась база и небольшие доработки

--- client.py
+++ client.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python
 import socket
-import sys
+import sys, select
+import time
+import threading
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QErrorMessage
-from Crypto.Cipher import DES
+
+
+class StoppableThread(threading.Thread):
+    #  Thread class with a stop() method.
+    def __init__(self, *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
+        self._stop_event = Event()
 
 
 class MyWidget(QMainWindow):
@@ -12,64 +21,49 @@ class MyWidget(QMainWindow):
         self.localIP = socket.gethostbyname(socket.getfqdn())
         self.pushButton.clicked.connect(self.run)
         self.lineEdit_9.setText(self.localIP)
-        self.localPort = 20001
-        self.bufferSize = 1024
-        self.UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.UDPServerSocket.bind((self.localIP, self.localPort))
+        host = self.lineEdit_4
+        port = int(self.lineEdit_6.text())
 
-    def pad(self, text):
-        if not isinstance(bytes, text):
-            text = text.encode('utf-8')
-            print('1')
-        while len(text) % 8 != 0:
-            text += b' '
-        return text
+        self.send_address = (host, port)  # Set the address to send to
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create Socket
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Make Socket Reusable
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Allow incoming broadcasts
+        self.s.setblocking(False)  # Socket non-block
+        self.s.bind(('', port))  # Binding port
 
-
-    def encrypt(self, text, key):
-        key = self.pad(key)
-        des = DES.new(key, DES.MODE_ECB)
-        text = self.pad(text)
-        print(text)
-        return des.encrypt(text)
+    # def main(self):
+    #     while True:
+    #         time.sleep(1)
+    #         try:
+    #             message, address = self.s.recvfrom(1024)  # Buffer size
+    #             if message:
+    #                 self.show(address + " >  " + message)  # Show this message
+    #         except:
+    #             pass
 
     def run(self):
-        global UDPServerSocket
-        text = self.encrypt(self.lineEdit.text(), self.lineEdit_7.text())
-        ip = self.lineEdit_4.text()
-        port = self.lineEdit_6.text()
-        data = self.encrypt(text, self.lineEdit_7.text())
-        data = data.decode('utf-8')
-        print(data)
-        UDPServerSocket.sendto(data, [ip, port])
-        print('sent')
-        # except Exception:
-        #     print(0)
-        #     error_dialog = QErrorMessage()
-        #     error_dialog.showMessage('Error. Check the fields.')
+        input = self.lineEdit.text()
+        if (input != ''):
+            self.s.sendto(input, self.send_address)
+            self.show('Me >  ' + input)
 
-
-def decrypt(text, key):
-    try:
-        des = DES.new(key, DES.MODE_ECB)
-        return des.decrypt(text).decode('utf-8')
-    except Exception:
-        return 'PYTHON EXCEPTION!!!'
-
-
-def show_message(text, key):
-    pass  # доделать
-
-
-# def answerer():
-#     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-#     message = bytesAddressPair[0]
-#     address = bytesAddressPair[1]
-#     print(clientMsg)
-#     UDPServerSocket.sendto(bytesToSend, address)
-
+    def show(self, text):
+        self.textBrowser.setText(self.textBrowser.setText() + '\n' + text)
 
 app = QApplication(sys.argv)
 ex = MyWidget()
+
+def main():
+    global ex
+    while True:
+        time.sleep(1)
+        try:
+            message, address = ex.s.recvfrom(1024)  # Buffer size
+            if message:
+                ex.show(address + " >  " + message)  # Show this message
+        except:
+            pass
+
+thr = threading.Thread(main)
 ex.show()
 sys.exit(app.exec_())
