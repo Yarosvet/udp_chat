5102355b694341f6bc19d1f7b8f92f9fa9f2a63d
Yarosvet <yarosvetk@yandex.ru>
2019-11-09 16:32:51 +0300

    Test-Version

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import socket
 import sys, select
 import time
 import threading
-from simple_thread import SimpleThread
+# from simple_thread import SimpleThread
 from PyQt5 import uic
 import sqlite3
 from PyQt5.QtCore import QThread
@@ -33,11 +33,13 @@ class MyWidget(QMainWindow):
         self.s.setblocking(False)  # Socket non-block
         self.s.bind(('', port))  # Binding port
         self.text_on_browser = ''
+        thr = threading.Thread(target=self.main)
+        thr.start()
         # self.thr = QThread()
         # self.thr.started.connect(self.main)
         # self.thr.thread(self)
         # self.thr.start()
-        self.main(thr_start=True)
+        # self.main(thr_start=True)
 
     def change_socket(self):
         host = self.lineEdit_4.text()
@@ -50,29 +52,24 @@ class MyWidget(QMainWindow):
         self.s.setblocking(False)  # Socket non-block
         self.s.bind((self.lineEdit_9.text(), port))
 
-    @SimpleThread
     def main(self):
         while True:
             try:
+                time.sleep(0.1)
                 message, address = self.s.recvfrom(1024)  # Buffer size
                 message = message.decode('utf-8')
-                print('decoded')
-                text = str(address) + " >  " + message
-                self.show_message(text)
+                self.plainText.appendPlainText(f'{address[0]}:{address[1]} >  {message}')
                 self.log_db(address, self.send_address[1], message)
             except:
                 pass
 
     def run(self):
+        self.send_address = (self.lineEdit_4.text(), int(self.lineEdit_6.text()))
         input = str(self.lineEdit.text())
         if (input != ''):
             self.s.sendto(bytes(input, 'utf-8'), self.send_address)  # sending text
-            self.show_message('Me >  ' + input)  # Show this message
+            self.plainText.appendPlainText('Me >  ' + input)  # Show this message
             self.log_db(self.lineEdit_9.text() + ' (Me)', self.send_address[1], input)
-
-    def show_message(self, text):
-        self.text_on_browser += '\n' + text
-        self.textBrowser.setText(self.text_on_browser)
 
     def log_db(self, ip, port, message):
         self.cur.execute(f"INSERT INTO messages(Ip, Port, Text) VALUES(\'{ip}\', {port}, \'{message}\')")
