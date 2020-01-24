210110020eeb2f3628789fa5a25367b7e827cc53
Yarosvet <000xsz839>
2020-01-24 15:59:04 +0300

    Removed Sql DB, Added RSA Encryption, and some other changes

--- client.py
+++ client.py
@@ -4,28 +4,37 @@ import sys, select
 import time
 import threading
 from pickle import loads, dumps
-# Qt and sql
+import rsa
+# Qt
 from PyQt5 import uic
-import sqlite3
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QErrorMessage
+
+
+def encrypt(message, public):
+    return rsa.encrypt(message, public)
+
+
+def decrypt(message, private):
+    return rsa.decrypt(message, private)
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
+        self.was_visible = False
         super().__init__()
         # Инициализация окна
         uic.loadUi('client.ui', self)
         self.localIP = socket.gethostbyname(socket.getfqdn())
         self.pushButton.clicked.connect(self.send_message)
         self.pushButton_2.clicked.connect(self.change_socket)
-        self.lineEdit_9.setText(self.localIP)
+        self.reciever_ip.textChanged.connect(self.reconnect)
+        self.local_ip.setText(self.localIP)
 
-        # Database
-        self.con = sqlite3.connect("messages.db")
-        self.cur = self.con.cursor()
+        host = self.reciever_ip.text()
+        port = int(self.port.text())
 
-        host = self.lineEdit_4.text()
-        port = int(self.lineEdit_6.text())
+        (self.pubkey, self.privkey) = rsa.newkeys(512)
+        self.pubkeys = {}
 
         self.send_address = (host, port)  # Set the address
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create Socket
@@ -38,41 +47,61 @@ class MyWidget(QMainWindow):
         thr.start()
 
     def change_socket(self):
-        host = self.lineEdit_4.text()
-        port = int(self.lineEdit_6.text())
+        host = self.reciever_ip.text()
+        port = int(self.port.text())
         self.send_address = (host, port)  # Set the address
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create Socket
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Make Socket Reusable
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Allow incoming broadcasts
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024)
         self.s.setblocking(False)  # Socket non-block
-        self.s.bind((self.lineEdit_9.text(), port))
+        self.s.bind((self.local_ip.text(), port))
+
+    def reconnect(self):
+        try:
+            send_address = (self.reciever_ip.text(), int(self.port.text()))
+            request = {'type': 'connect', 'data': {'pubkey': self.pubkey, 'need_for_answer': 'True'}}
+            self.s.sendto(dumps(request), send_address)
+        except socket.gaierror:
+            pass
 
     def main(self):
         while True:
+            if not self.isVisible() and self.was_visible:
+                break
+            else:
+                self.was_visible = True
             try:
                 time.sleep(0.1)
                 message, address = self.s.recvfrom(1024)  # Buffer size
                 request = loads(message)
                 if request['type'] == 'message':
-                    text = request['text']
+                    data = loads(decrypt(request['data'], self.privkey))
+                    text = data['text']
                     self.plainText.appendPlainText(f'{address[0]}:{address[1]} >  {text}')  # printing message
-                    self.log_db(address, self.send_address[1], text)
-            except:
+                elif request['type'] == 'connect':
+                    if address[0] == '127.0.0.1':
+                        self.pubkeys['127.0.1.1'] = request['data']['pubkey']
+                    self.pubkeys[address[0]] = request['data']['pubkey']
+                    if request['data']['need_for_answer'] == 'True':
+                        request = {'type': 'connect', 'data': {'pubkey': self.pubkey, 'need_for_answer': 'False'}}
+                        self.s.sendto(dumps(request), address)
+            except BlockingIOError:
                 pass
 
     def send_message(self):
-        self.send_address = (self.lineEdit_4.text(), int(self.lineEdit_6.text()))
+        self.send_address = (self.reciever_ip.text(), int(self.port.text()))
         input = str(self.lineEdit.text())
-        if (input != ''):
-            request = {'type': 'message', 'text': input}
-            self.s.sendto(dumps(request), self.send_address)  # sending text
-            self.plainText.appendPlainText('Me >  ' + input)  # Show this message
-            self.log_db(self.lineEdit_9.text() + ' (Me)', self.send_address[1], input)
-
-    def log_db(self, ip, port, message):
-        self.cur.execute(f"INSERT INTO messages(Ip, Port, Text) VALUES(\'{ip}\', {port}, \'{message}\')")
-        self.con.commit()
+        request = {'type': 'connect', 'data': {'pubkey': self.pubkey, 'need_for_answer': 'True'}}
+        self.s.sendto(dumps(request), self.send_address)
+        try:
+            if input != '':
+                crypted = encrypt(dumps({'text': input}), self.pubkeys[self.send_address[0]])
+                request = {'type': 'message', 'data': crypted}
+                self.s.sendto(dumps(request), self.send_address)  # sending text
+                self.plainText.appendPlainText('Me >  ' + input)  # Show this message
+        except:
+            pass
 
 
 app = QApplication(sys.argv)
